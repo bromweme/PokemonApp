a14b765121cb852216b7ba9b32e12460bb2c0816
Michael Bromwell <bromweme@gmail.com>
2021-03-25 11:55:51 -0400

    Structure of program added

--- PokemonApp/PokeApp.Designer.vb
+++ PokemonApp/PokeApp.Designer.vb
@@ -27,13 +27,21 @@ Partial Class PokeApp
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.cboSeries = New System.Windows.Forms.ComboBox()
+        Me.cboSets = New System.Windows.Forms.ComboBox()
+        Me.cboCards = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.lblRarity = New System.Windows.Forms.Label()
+        Me.txtCount = New System.Windows.Forms.TextBox()
+        Me.webImage = New System.Windows.Forms.WebBrowser()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(403, 51)
+        Me.Label1.Location = New System.Drawing.Point(33, 72)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(107, 13)
         Me.Label1.TabIndex = 0
@@ -42,7 +50,7 @@ Partial Class PokeApp
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(403, 85)
+        Me.Label2.Location = New System.Drawing.Point(33, 107)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(86, 13)
         Me.Label2.TabIndex = 1
@@ -51,7 +59,7 @@ Partial Class PokeApp
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(403, 124)
+        Me.Label3.Location = New System.Drawing.Point(33, 142)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(95, 13)
         Me.Label3.TabIndex = 2
@@ -60,7 +68,7 @@ Partial Class PokeApp
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(403, 160)
+        Me.Label4.Location = New System.Drawing.Point(33, 177)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(37, 13)
         Me.Label4.TabIndex = 3
@@ -69,27 +77,99 @@ Partial Class PokeApp
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(403, 193)
+        Me.Label5.Location = New System.Drawing.Point(33, 212)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(112, 13)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "Number of Duplicates:"
         '
-        'Button1
+        'btnSubmit
         '
-        Me.Button1.Location = New System.Drawing.Point(52, 296)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(181, 88)
-        Me.Button1.TabIndex = 5
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnSubmit.Location = New System.Drawing.Point(36, 309)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(181, 88)
+        Me.btnSubmit.TabIndex = 5
+        Me.btnSubmit.Text = "Submit Changes"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'cboSeries
+        '
+        Me.cboSeries.FormattingEnabled = True
+        Me.cboSeries.Location = New System.Drawing.Point(153, 29)
+        Me.cboSeries.Name = "cboSeries"
+        Me.cboSeries.Size = New System.Drawing.Size(280, 21)
+        Me.cboSeries.TabIndex = 6
+        '
+        'cboSets
+        '
+        Me.cboSets.FormattingEnabled = True
+        Me.cboSets.Location = New System.Drawing.Point(153, 64)
+        Me.cboSets.Name = "cboSets"
+        Me.cboSets.Size = New System.Drawing.Size(280, 21)
+        Me.cboSets.TabIndex = 7
+        '
+        'cboCards
+        '
+        Me.cboCards.FormattingEnabled = True
+        Me.cboCards.Location = New System.Drawing.Point(153, 99)
+        Me.cboCards.Name = "cboCards"
+        Me.cboCards.Size = New System.Drawing.Size(280, 21)
+        Me.cboCards.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(33, 37)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(87, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Pokemon Series:"
+        '
+        'lblNumber
+        '
+        Me.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNumber.Location = New System.Drawing.Point(172, 132)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(100, 23)
+        Me.lblNumber.TabIndex = 13
+        '
+        'lblRarity
+        '
+        Me.lblRarity.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblRarity.Location = New System.Drawing.Point(172, 167)
+        Me.lblRarity.Name = "lblRarity"
+        Me.lblRarity.Size = New System.Drawing.Size(100, 23)
+        Me.lblRarity.TabIndex = 14
+        '
+        'txtCount
+        '
+        Me.txtCount.Location = New System.Drawing.Point(172, 212)
+        Me.txtCount.Name = "txtCount"
+        Me.txtCount.Size = New System.Drawing.Size(100, 20)
+        Me.txtCount.TabIndex = 15
+        '
+        'webImage
+        '
+        Me.webImage.Location = New System.Drawing.Point(492, 29)
+        Me.webImage.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.webImage.Name = "webImage"
+        Me.webImage.Size = New System.Drawing.Size(250, 250)
+        Me.webImage.TabIndex = 16
         '
         'PokeApp
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.webImage)
+        Me.Controls.Add(Me.txtCount)
+        Me.Controls.Add(Me.lblRarity)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.cboCards)
+        Me.Controls.Add(Me.cboSets)
+        Me.Controls.Add(Me.cboSeries)
+        Me.Controls.Add(Me.btnSubmit)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -107,5 +187,13 @@ Partial Class PokeApp
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents cboSeries As ComboBox
+    Friend WithEvents cboSets As ComboBox
+    Friend WithEvents cboCards As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblNumber As Label
+    Friend WithEvents lblRarity As Label
+    Friend WithEvents txtCount As TextBox
+    Friend WithEvents webImage As WebBrowser
 End Class
--- PokemonApp/PokeApp.vb
+++ PokemonApp/PokeApp.vb
@@ -1,3 +1,356 @@
-﻿Public Class PokeApp
+﻿Option Strict On
+Public Class PokeApp
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+            Try
+
+                Dim strSelect As String = ""
+                Dim cmdSelect As OleDb.OleDbCommand ' this will be used for our Select statement
+                Dim drSourceTable As OleDb.OleDbDataReader ' this will be where our data is retrieved to
+                Dim dt As DataTable = New DataTable ' this is the table we will load from our reader
+
+                ' loop through the textboxes and clear them in case they have data in them after a delete
+                For Each cntrl As Control In Controls
+                    If TypeOf cntrl Is TextBox Then
+                        cntrl.Text = String.Empty
+                    End If
+                Next
+
+                ' open the DB
+                If OpenDatabaseConnectionSQLServer() = False Then
+
+                    ' No, warn the user ...
+                    MessageBox.Show(Me, "Database connection error." & vbNewLine &
+                                        "The application will now close.",
+                                        Me.Text + " Error",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                    ' and close the form/application
+                    Me.Close()
+
+                End If
+
+            ' Build the select statement
+            strSelect = "SELECT id, name FROM card_series"
+
+            ' Retrieve all the records 
+            cmdSelect = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
+                drSourceTable = cmdSelect.ExecuteReader
+
+                ' load table from data reader
+                dt.Load(drSourceTable)
+
+            ' Add the item to the combo box. We need the player ID associated with the name so 
+            ' when we click on the name we can then use the ID to pull the rest of the players data.
+            ' We are binding the column name to the combo box display and value members. 
+            cboSeries.ValueMember = "id"
+            cboSeries.DisplayMember = "name"
+            cboSeries.DataSource = dt
+
+            ' Select the first item in the list by default
+            If cboSeries.Items.Count > 0 Then cboSeries.SelectedIndex = 0
+
+            ' Clean up
+            drSourceTable.Close()
+
+                ' close the database connection
+                CloseDatabaseConnection()
+
+            Catch ex As Exception
+
+                ' Log and display error message
+                MessageBox.Show(ex.Message)
+
+            End Try
+
+        End Sub
+        Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim strSelect As String = ""
+        Dim intCount As Integer
+        Dim intRowsAffected As Integer
+
+        Try
+
+
+
+            ' thie will hold our Update statement
+            Dim cmdUpdate As OleDb.OleDbCommand
+
+            ' check to make sure all text boxes have data. No data no update!
+            If Validation() = True Then
+                ' open database
+                If OpenDatabaseConnectionSQLServer() = False Then
+
+                    ' No, warn the user ...
+                    MessageBox.Show(Me, "Database connection error." & vbNewLine &
+                                        "The application will now close.",
+                                        Me.Text + " Error",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                    ' and close the form/application
+                    Me.Close()
+
+                End If
+
+                ' after you validate there is data put values into variables
+                If Validation() = True Then
+
+                    intCount = CInt(txtCount.Text)
+
+
+                    ' Build the select statement using PK from name selected
+                    strSelect = "Update cards Set intCount = '" & intCount & "' ""Where id = " & cboCards.SelectedValue.ToString
+
+                    ' uncomment out the following message box line to use as a tool to check your sql statement
+                    ' remember anything not a numeric value going into SQL Server must have single quotes '
+                    ' around it, including dates.
+
+                    'MessageBox.Show(strSelect)
+
+
+                    ' make the connection
+                    cmdUpdate = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
+
+                    ' IUpdate the row with execute the statement
+                    intRowsAffected = cmdUpdate.ExecuteNonQuery()
+
+                    ' have to let the user know what happened 
+                    If intRowsAffected = 1 Then
+                        MessageBox.Show("Update successful")
+                    Else
+                        MessageBox.Show("Update failed")
+                    End If
+
+                    ' close the database connection
+                    CloseDatabaseConnection()
+
+                    ' call the Form Load sub to refresh the combo box data after a delete
+                    Form1_Load(sender, e)
+                End If
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+    Public Function Validation() As Boolean
+
+        ' loop through the textboxes and check to make sure there is data in them
+        For Each cntrl As Control In Controls
+            If TypeOf cntrl Is TextBox Then
+                cntrl.BackColor = Color.White
+                If cntrl.Text = String.Empty Then
+                    cntrl.BackColor = Color.Yellow
+                    cntrl.Focus()
+                    Return False
+                End If
+            End If
+        Next
+
+        'every this is good so return true
+        Return True
+
+    End Function
+    'Private Sub cboCards_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboCards.SelectedIndexChanged
+
+    '    Dim strSelect As String = ""
+    '    Dim strName As String = ""
+    '    Dim cmdSelect As OleDb.OleDbCommand ' this will be used for our Select statement
+    '    Dim drSourceTable As OleDb.OleDbDataReader ' this will be where our data is retrieved to
+    '    Dim dt As DataTable = New DataTable ' this is the table we will load from our reader
+
+    '    Try
+
+
+
+    '        ' open the database
+    '        If OpenDatabaseConnectionSQLServer() = False Then
+
+    '            ' No, warn the user ...
+    '            MessageBox.Show(Me, "Database connection error." & vbNewLine &
+    '                                "The application will now close.",
+    '                                Me.Text + " Error",
+    '                                MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+    '            ' and close the form/application
+    '            Me.Close()
+
+    '        End If
+
+    '        ' Build the select statement using PK from name selected
+    '        strSelect = "SELECT strFirstName, strLastName, strAddress, strCity, strState, strZip FROM TPlayers Where intPlayerID = " & cboSeries.SelectedValue.ToString
+
+    '        ' Retrieve all the records 
+    '        cmdSelect = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
+    '        drSourceTable = cmdSelect.ExecuteReader
+
+    '        ' load the data table from the reader
+    '        dt.Load(drSourceTable)
+
+    '        ' populate the text boxes with the data
+    '        'lbl.Text = dt.Rows(0).Item(0).ToString
+    '        ' txtLastName.Text = dt.Rows(0).Item(1).ToString
+    '        'txtAddress.Text = dt.Rows(0).Item(2).ToString
+    '        'txtCity.Text = dt.Rows(0).Item(3).ToString
+    '        'txtState.Text = dt.Rows(0).Item(4).ToString
+    '        'txtZip.Text = dt.Rows(0).Item(5).ToString
+
+    '        ' close the database connection
+    '        CloseDatabaseConnection()
+
+
+    '    Catch ex As Exception
+    '        MessageBox.Show(ex.Message)
+    '    End Try
+    'End Sub
+
+    Private Sub cboSeries_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboSeries.SelectedIndexChanged
+
+        Try
+
+            Dim strSelect As String = ""
+            Dim cmdSelect As OleDb.OleDbCommand ' this will be used for our Select statement
+            Dim drSourceTable As OleDb.OleDbDataReader ' this will be where our data is retrieved to
+            Dim dt As DataTable = New DataTable ' this is the table we will load from our reader
+
+            ' loop through the textboxes and clear them in case they have data in them after a delete
+            For Each cntrl As Control In Controls
+                If TypeOf cntrl Is TextBox Then
+                    cntrl.Text = String.Empty
+                End If
+            Next
+
+            ' open the DB
+            If OpenDatabaseConnectionSQLServer() = False Then
+
+                ' No, warn the user ...
+                MessageBox.Show(Me, "Database connection error." & vbNewLine &
+                                        "The application will now close.",
+                                        Me.Text + " Error",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                ' and close the form/application
+                Me.Close()
+
+            End If
+
+            ' Build the select statement
+            strSelect = "SELECT id, name FROM card_sets WHERE series_id = " & cboSeries.SelectedValue.ToString
+
+            ' Retrieve all the records 
+            cmdSelect = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
+            drSourceTable = cmdSelect.ExecuteReader
+
+            ' load table from data reader
+            dt.Load(drSourceTable)
+
+            ' Add the item to the combo box. We need the player ID associated with the name so 
+            ' when we click on the name we can then use the ID to pull the rest of the players data.
+            ' We are binding the column name to the combo box display and value members. 
+            cboSets.ValueMember = "id"
+            cboSets.DisplayMember = "name"
+            cboSets.DataSource = dt
+
+            ' Select the first item in the list by default
+            If cboSets.Items.Count > 0 Then cboSets.SelectedIndex = 0
+
+            ' Clean up
+            drSourceTable.Close()
+
+            ' close the database connection
+            CloseDatabaseConnection()
+
+        Catch ex As Exception
+
+            ' Log and display error message
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+    Private Sub cboSets_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboSets.SelectedIndexChanged
+
+        Try
+
+            Dim strSelect As String = ""
+            Dim cmdSelect As OleDb.OleDbCommand ' this will be used for our Select statement
+            Dim drSourceTable As OleDb.OleDbDataReader ' this will be where our data is retrieved to
+            Dim dt As DataTable = New DataTable ' this is the table we will load from our reader
+
+            ' loop through the textboxes and clear them in case they have data in them after a delete
+            For Each cntrl As Control In Controls
+                If TypeOf cntrl Is TextBox Then
+                    cntrl.Text = String.Empty
+                End If
+            Next
+
+            ' open the DB
+            If OpenDatabaseConnectionSQLServer() = False Then
+
+                ' No, warn the user ...
+                MessageBox.Show(Me, "Database connection error." & vbNewLine &
+                                        "The application will now close.",
+                                        Me.Text + " Error",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                ' and close the form/application
+                Me.Close()
+
+            End If
+
+            ' Build the select statement
+            strSelect = "SELECT id, name, image FROM cards WHERE set_id = " & cboSets.SelectedValue.ToString
+
+            ' Retrieve all the records 
+            cmdSelect = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
+            drSourceTable = cmdSelect.ExecuteReader
+
+            ' load table from data reader
+            dt.Load(drSourceTable)
+
+            ' Add the item to the combo box. We need the player ID associated with the name so 
+            ' when we click on the name we can then use the ID to pull the rest of the players data.
+            ' We are binding the column name to the combo box display and value members. 
+            cboCards.ValueMember = "id"
+            cboCards.DisplayMember = "name"
+            cboCards.DataSource = dt
+
+            ' Select the first item in the list by default
+            If cboCards.Items.Count > 0 Then cboCards.SelectedIndex = 0
+
+            drSourceTable.Close()
+
+            strSelect = "SELECT image FROM cards Where id = " & cboCards.SelectedValue.ToString
+
+            'Retrieve all the records 
+            cmdSelect = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
+            drSourceTable = cmdSelect.ExecuteReader
+
+            'load the data table from the reader
+            dt.Load(drSourceTable)
+
+            'populate the text boxes with the data
+            webImage.Navigate(dt.Rows(0).Item(4).ToString)
+
+
+            drSourceTable.Close()
+
+
+            ' close the database connection
+            CloseDatabaseConnection()
+
+        Catch ex As Exception
+
+            ' Log and display error message
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+
 
 End Class
